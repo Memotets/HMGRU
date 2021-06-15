6fbaa0a9b7868ca627525f7d92c70f31965b6c4a
Memotets <guillermo.bg99@gmail.com>
2021-06-15 03:12:27 -0600

    Intento de conexion de edificios a vista

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from pages.views import GeneralGraphView
+from pages.views import BuildingGraphView
 #from pages.APIViews.datoGeneralView import DatosGeneralView
 
 urlpatterns = [
     path('', GeneralGraphView),
-    #path('APIweb/', DatosGeneralView.as_view()),
+    path('edificios/', BuildingGraphView),
 ]
