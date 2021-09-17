ab93d50ab222eb8d95f439f6f69a29a04499817a
odiseo <orlando.bel.jimmy@gmail.com>
2021-09-17 00:15:45 -0500

    Cambios generales para inicio ciclo 5

--- hmgru/settings.py
+++ hmgru/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a&(l6)y&dg##)syugbn04jyu4h4i%wn#gng&kfur&vabs578!1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 	"localhost",
@@ -123,10 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATIC_URL = '/pages/templates/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/home/upiiz/Documents/sistemas/hmgru/static/')
+print( STATIC_ROOT)
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'pages/templates/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
